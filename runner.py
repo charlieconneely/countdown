73d894f5c9a47636784658f700e8da52f7f941e5
Charlie Conneely <charlieconneely.cc@gmail.com>
2020-06-07 17:26:07 +0100

    comments added input errors corrected

--- runner.py
+++ runner.py
@@ -8,31 +8,40 @@ def main(name):
     words = dictionary_parser.parse("words.txt")
     found = True
 
+    # retrieve array of letters
     nine_letters = rachel()
     # convert array to string
     nine_letters_string = ''.join(nine_letters)
     # convert to uppercase
-    nine_letters_string = nine_letters_string.upper()
+    #nine_letters_string = nine_letters_string.upper()
     print(nine_letters_string)
 
     input_word = input("Enter your word: ")
-    input_word = input_word.lower()
+    # convert to upper case
+    input_word = input_word.upper()
+    # convert word to array of chars for inspection
     input_word_split = split(input_word)
 
-    # Check if each character sees itself in the nine_letters
+    # check if each letter sees itself on the board
     for char in input_word_split:
+        # check if the letter is present on the board
         if char in nine_letters:
+            # remove the letter from the board
             nine_letters.remove(char)
         else:
             found = False
 
     if found:
+        # convert word back to lower
+        input_word = input_word.lower()
+        # check if the word is in the dictionary
         if input_word in words:
             if (len(input_word) < 6):
-                print("Good job "+str(name)+"! Length: " +\
+                print("Good job "+name+"! Length: " +\
                         str(len(input_word)))
             else:
-                print("Great word! Length: " + str(len(input_word)))
+                print("Great word "+name+"! Length: "\
+                        + str(len(input_word)))
         else:
             print("Word not found in dictionary")
     else:
@@ -45,9 +54,9 @@ def split(word):
 def rachel():
     x = 0
     letters = []
-    vowels = ['a','e','i','o','u']
-    consonants = ['b','c','d','f','g','h','j','k','l','m',
-            'n','p','q','r','s','t','v','x','z','w','y']
+    vowels = ['A','E','I','O','U']
+    consonants = ['B','C','D','F','G','H','J','K','L','M',
+            'N','P','Q','R','S','T','V','X','Z','W','Y']
 
     print("\nRachel, the letters please!\n")
 
@@ -55,14 +64,20 @@ def rachel():
         choice = input("Vowel or Consonant? (v/c): ")
         choice = choice.lower()
         if choice == 'v':
+            # generate random number
             letternum = random.randint(0, len(vowels)-1)
+            # select vowel at that index
             letter = vowels[letternum]
+            # append letter to list of letters
             letters.append(letter)
-            print(letter)
             x = x + 1
+            print(letter)
         elif choice == 'c':
+            # generate random number
             letternum = random.randint(0, len(consonants)-1)
+            # select consonant at that index
             letter = consonants[letternum]
+            # append letter to list of letters
             letters.append(letter)
             print(letter)
             x = x + 1
@@ -71,16 +86,21 @@ def rachel():
     return letters
 
 def intro():
-    print("\nLet's Play Countdown!\n")
-    name = input("Enter your name here: ")
+    name = str(input("Enter your name here: "))
+    print("Welcome "+name+"!!\nLet's Play Countdown!\n")
     return name
 
 if __name__ == "__main__":
     name = intro()
     while True:
         main(name)
-        choice = input("\nWould you like to play again? (y/n):")
-        choice = choice.lower()
+        while True:
+            choice = input("\nWould you like to play again? (y/n):")
+            choice = choice.lower()
+            if choice == 'n' or choice == 'y':
+                break
+            else:
+                print("Invalid input. Please try again.")
         if (choice == 'n'):
             break
 
